876c15d2dd4feb3c8f493dfe4b7c08d354b29f65
Lars <lars.bogatz@gmail.com>
2025-10-28 20:23:51 +0100

    Updated main and join listener for new structure, apis etc.

--- src/main/java/de/lars/apimanager/Main.java
+++ src/main/java/de/lars/apimanager/Main.java
@@ -1,132 +1,165 @@
-package de.lars.apiManager;
+package de.lars.apimanager;
 
-import de.lars.apiManager.backpackAPI.BackpackAPI;
-import de.lars.apiManager.backpackAPI.BackpackAPIImpl;
-import de.lars.apiManager.banAPI.BanAPI;
-import de.lars.apiManager.banAPI.BanAPIImpl;
-import de.lars.apiManager.chunkAPI.ChunkAPI;
-import de.lars.apiManager.chunkAPI.ChunkAPIImpl;
-import de.lars.apiManager.coinAPI.CoinAPI;
-import de.lars.apiManager.coinAPI.CoinAPIImpl;
-import de.lars.apiManager.dataAPI.DataAPI;
-import de.lars.apiManager.dataAPI.DataAPIImpl;
-import de.lars.apiManager.database.MySQL;
-import de.lars.apiManager.homeAPI.HomeAPI;
-import de.lars.apiManager.homeAPI.HomeAPIImpl;
-import de.lars.apiManager.languageAPI.LanguageAPI;
-import de.lars.apiManager.languageAPI.LanguageAPIImpl;
-import de.lars.apiManager.listeners.JoinListener;
-import de.lars.apiManager.playersAPI.PlayerAPI;
-import de.lars.apiManager.playersAPI.PlayerAPIImpl;
-import de.lars.apiManager.questAPI.QuestAPI;
-import de.lars.apiManager.questAPI.QuestAPIImpl;
-import de.lars.apiManager.rankAPI.RankAPI;
-import de.lars.apiManager.rankAPI.RankAPIImpl;
-import de.lars.apiManager.timerAPI.TimerAPI;
-import de.lars.apiManager.timerAPI.TimerAPIImpl;
-import de.lars.apiManager.toggleAPI.ToggleAPI;
-import de.lars.apiManager.toggleAPI.ToggleAPIImpl;
+import de.lars.apimanager.apis.backpackAPI.BackpackAPI;
+import de.lars.apimanager.apis.backpackAPI.BackpackAPIImpl;
+import de.lars.apimanager.apis.banAPI.BanAPI;
+import de.lars.apimanager.apis.banAPI.BanAPIImpl;
+import de.lars.apimanager.apis.coinAPI.CoinAPI;
+import de.lars.apimanager.apis.coinAPI.CoinAPIImpl;
+import de.lars.apimanager.apis.courtAPI.CourtAPI;
+import de.lars.apimanager.apis.courtAPI.CourtAPIImpl;
+import de.lars.apimanager.apis.languageAPI.LanguageAPI;
+import de.lars.apimanager.apis.languageAPI.LanguageAPIImpl;
+import de.lars.apimanager.apis.limitAPI.LimitAPI;
+import de.lars.apimanager.apis.limitAPI.LimitAPIImpl;
+import de.lars.apimanager.apis.nickAPI.NickAPI;
+import de.lars.apimanager.apis.nickAPI.NickAPIImpl;
+import de.lars.apimanager.apis.prefixAPI.PrefixAPI;
+import de.lars.apimanager.apis.prefixAPI.PrefixAPIImpl;
+import de.lars.apimanager.apis.questAPI.QuestAPI;
+import de.lars.apimanager.apis.questAPI.QuestAPIImpl;
+import de.lars.apimanager.apis.rankAPI.RankAPI;
+import de.lars.apimanager.apis.rankAPI.RankAPIImpl;
+import de.lars.apimanager.apis.serverSettingsAPI.ServerSettingsAPI;
+import de.lars.apimanager.apis.serverSettingsAPI.ServerSettingsAPIImpl;
+import de.lars.apimanager.apis.playerAPI.PlayerAPI;
+import de.lars.apimanager.apis.playerAPI.PlayerAPIImpl;
+import de.lars.apimanager.apis.statusAPI.StatusAPI;
+import de.lars.apimanager.apis.statusAPI.StatusAPIImpl;
+import de.lars.apimanager.apis.timerAPI.TimerAPI;
+import de.lars.apimanager.apis.timerAPI.TimerAPIImpl;
+import de.lars.apimanager.apis.toggleAPI.ToggleAPI;
+import de.lars.apimanager.apis.toggleAPI.ToggleAPIImpl;
+import de.lars.apimanager.database.DatabaseManager;
+import de.lars.apimanager.listeners.JoinListener;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Main extends JavaPlugin {
-
     private static Main instance;
-    private MySQL mySQL;
-    private PlayerAPIImpl playerAPI;
-    private CoinAPIImpl coinAPI;
-    private LanguageAPIImpl languageAPI;
-    private RankAPIImpl rankAPI;
-    private ToggleAPIImpl toggleAPI;
-    private HomeAPIImpl homeAPI;
-    private QuestAPIImpl questAPI;
-    private BanAPIImpl banAPI;
-    private ChunkAPIImpl chunkAPI;
-    private TimerAPIImpl timerAPI;
-    private BackpackAPIImpl backpackAPI;
-    private DataAPIImpl dataAPI;
+    private DatabaseManager databaseManager;
 
+    private ServerSettingsAPIImpl serverSettingsAPI;
+    private PlayerAPIImpl playerAPI;
+    private LanguageAPIImpl languageAPI;
+    private BackpackAPIImpl backpackAPI;
+    private LimitAPIImpl limitAPI;
+    private BanAPIImpl banAPI;
+    private CourtAPIImpl courtAPI;
+    private RankAPIImpl rankAPI;
+    private PrefixAPIImpl prefixAPI;
+    private StatusAPIImpl statusAPI;
+    private NickAPIImpl nickAPI;
+    private ToggleAPIImpl toggleAPI;
+    private CoinAPIImpl coinAPI;
+    private QuestAPIImpl questAPI;
+    private TimerAPIImpl timerAPI;
 
     @Override
     public void onLoad() {
         instance = this;
-
         saveDefaultConfig();
 
         String host = getConfig().getString("database.host");
         int port = getConfig().getInt("database.port");
         String database = getConfig().getString("database.database");
-        String user = getConfig().getString("database.username");
+        String user = getConfig().getString("database.user");
         String password = getConfig().getString("database.password");
 
-        this.mySQL = MySQL.newBuilder()
-                .withUrl(host)
-                .withPort(port)
-                .withDatabase(database)
-                .withUser(user)
-                .withPassword(password)
-                .create();
+        this.databaseManager = new DatabaseManager(host, port, database, user, password);
     }
 
     @Override
     public void onEnable() {
+        serverSettingsAPI = new ServerSettingsAPIImpl();
+        ServerSettingsAPI.setApi(serverSettingsAPI);
+        serverSettingsAPI.createTables();
+
         playerAPI = new PlayerAPIImpl();
-        playerAPI.createTables();
         PlayerAPI.setApi(playerAPI);
-
-        backpackAPI = new BackpackAPIImpl();
-        backpackAPI.createTables();
-        BackpackAPI.setApi(backpackAPI);
-
-        coinAPI = new CoinAPIImpl();
-        coinAPI.createTables();
-        CoinAPI.setApi(coinAPI);
+        playerAPI.createTables();
 
         languageAPI = new LanguageAPIImpl();
-        languageAPI.createTables();
         LanguageAPI.setApi(languageAPI);
+        languageAPI.createTables();
 
-        rankAPI = new RankAPIImpl();
-        rankAPI.createTables();
-        RankAPI.setApi(rankAPI);
+        backpackAPI = new BackpackAPIImpl();
+        BackpackAPI.setApi(backpackAPI);
+        backpackAPI.createTables();
 
-        toggleAPI = new ToggleAPIImpl();
-        toggleAPI.createTables();
-        ToggleAPI.setApi(toggleAPI);
-
-        homeAPI = new HomeAPIImpl();
-        homeAPI.createTables();
-        HomeAPI.setApi(homeAPI);
-
-        questAPI = new QuestAPIImpl();
-        questAPI.createTables();
-        QuestAPI.setApi(questAPI);
+        limitAPI = new LimitAPIImpl();
+        LimitAPI.setApi(limitAPI);
+        limitAPI.createTables();
 
         banAPI = new BanAPIImpl();
-        banAPI.createTables();
         BanAPI.setApi(banAPI);
+        banAPI.createTables();
 
-        chunkAPI = new ChunkAPIImpl();
-        chunkAPI.createTables();
-        ChunkAPI.setApi(chunkAPI);
+        courtAPI = new CourtAPIImpl();
+        CourtAPI.setApi(courtAPI);
+        courtAPI.createTables();
+
+        rankAPI = new RankAPIImpl();
+        RankAPI.setApi(rankAPI);
+        rankAPI.createTables();
+
+        prefixAPI = new PrefixAPIImpl();
+        PrefixAPI.setApi(prefixAPI);
+        prefixAPI.createTables();
+
+        statusAPI = new StatusAPIImpl();
+        StatusAPI.setApi(statusAPI);
+        statusAPI.createTables();
+
+        nickAPI = new NickAPIImpl();
+        NickAPI.setApi(nickAPI);
+        nickAPI.createTables();
+
+        toggleAPI = new ToggleAPIImpl();
+        ToggleAPI.setApi(toggleAPI);
+        toggleAPI.createTables();
+
+        coinAPI = new CoinAPIImpl();
+        CoinAPI.setApi(coinAPI);
+        coinAPI.createTables();
+
+        questAPI = new QuestAPIImpl();
+        QuestAPI.setApi(questAPI);
+        questAPI.createTables();
 
         timerAPI = new TimerAPIImpl();
-        timerAPI.createTables();
         TimerAPI.setApi(timerAPI);
-
-        dataAPI = new DataAPIImpl();
-        dataAPI.createTables();
-        DataAPI.setApi(dataAPI);
+        timerAPI.createTables();
 
         Bukkit.getPluginManager().registerEvents(new JoinListener(), this);
+
         Component message = Component.text()
                 .append(Component.text("[", NamedTextColor.DARK_GRAY))
                 .append(Component.text("ApiManager", NamedTextColor.GOLD))
                 .append(Component.text("]", NamedTextColor.DARK_GRAY))
                 .append(Component.text(" All APIs are ready!", NamedTextColor.DARK_GREEN))
                 .build();
+
+        Bukkit.getConsoleSender().sendMessage(message);
+    }
+
+    @Override
+    public void onDisable() {
+        if (databaseManager != null) {
+            databaseManager.close();
+        }
+
+        Bukkit.getPluginManager().registerEvents(new JoinListener(), this);
+
+        Component message = Component.text()
+                .append(Component.text("[", NamedTextColor.DARK_GRAY))
+                .append(Component.text("ApiManager", NamedTextColor.GOLD))
+                .append(Component.text("]", NamedTextColor.DARK_GRAY))
+                .append(Component.text(" Database successfully disdonnected!", NamedTextColor.DARK_GREEN))
+                .build();
+
         Bukkit.getConsoleSender().sendMessage(message);
     }
 
@@ -134,55 +167,67 @@ public final class Main extends JavaPlugin {
         return instance;
     }
 
-    public MySQL getMySQL() {
-        return mySQL;
+    public DatabaseManager getDatabaseManager() {
+        return databaseManager;
+    }
+
+    public ServerSettingsAPIImpl getServerSettingsAPI() {
+        return serverSettingsAPI;
     }
 
     public PlayerAPIImpl getPlayerAPI() {
         return playerAPI;
     }
 
-    public CoinAPIImpl getCoinAPI() {
-        return coinAPI;
-    }
-
     public LanguageAPIImpl getLanguageAPI() {
         return languageAPI;
-    }
-
-    public RankAPIImpl getRankAPI() {
-        return rankAPI;
-    }
-
-    public ToggleAPIImpl getToggleAPI() {
-        return toggleAPI;
-    }
-
-    public HomeAPIImpl getHomeAPI() {
-        return homeAPI;
-    }
-
-    public QuestAPIImpl getQuestAPI() {
-        return questAPI;
-    }
-
-    public BanAPIImpl getBanAPI() {
-        return banAPI;
-    }
-
-    public ChunkAPIImpl getChunkAPI() {
-        return chunkAPI;
-    }
-
-    public TimerAPIImpl getTimerAPI() {
-        return timerAPI;
     }
 
     public BackpackAPIImpl getBackpackAPI() {
         return backpackAPI;
     }
 
-    public DataAPIImpl getDataAPI() {
-        return dataAPI;
+    public LimitAPIImpl getLimitAPI() {
+        return limitAPI;
+    }
+
+    public BanAPIImpl getBanAPI() {
+        return banAPI;
+    }
+
+    public CourtAPIImpl getCourtAPI() {
+        return courtAPI;
+    }
+
+    public RankAPIImpl getRankAPI() {
+        return rankAPI;
+    }
+
+    public PrefixAPIImpl getPrefixAPI() {
+        return prefixAPI;
+    }
+
+    public StatusAPIImpl getStatusAPI() {
+        return statusAPI;
+    }
+
+    public NickAPIImpl getNickAPI() {
+        return nickAPI;
+    }
+
+    public ToggleAPIImpl getToggleAPI() {
+        return toggleAPI;
+    }
+
+    public CoinAPIImpl getCoinAPI() {
+        return coinAPI;
+    }
+
+    public QuestAPIImpl getQuestAPI() {
+        return questAPI;
+    }
+
+    public TimerAPIImpl getTimerAPI() {
+        return timerAPI;
     }
 }
--- src/main/java/de/lars/apimanager/listeners/JoinListener.java
+++ src/main/java/de/lars/apimanager/listeners/JoinListener.java
@@ -1,15 +1,20 @@
-package de.lars.apiManager.listeners;
+package de.lars.apimanager.listeners;
 
-import de.lars.apiManager.Main;
-import de.lars.apiManager.backpackAPI.BackpackAPIImpl;
-import de.lars.apiManager.banAPI.BanAPIImpl;
-import de.lars.apiManager.coinAPI.CoinAPIImpl;
-import de.lars.apiManager.languageAPI.LanguageAPIImpl;
-import de.lars.apiManager.playersAPI.PlayerAPIImpl;
-import de.lars.apiManager.questAPI.QuestAPIImpl;
-import de.lars.apiManager.rankAPI.RankAPIImpl;
-import de.lars.apiManager.timerAPI.TimerAPIImpl;
-import de.lars.apiManager.toggleAPI.ToggleAPIImpl;
+import de.lars.apimanager.Main;
+import de.lars.apimanager.apis.backpackAPI.BackpackAPIImpl;
+import de.lars.apimanager.apis.banAPI.BanAPIImpl;
+import de.lars.apimanager.apis.coinAPI.CoinAPIImpl;
+import de.lars.apimanager.apis.courtAPI.CourtAPIImpl;
+import de.lars.apimanager.apis.languageAPI.LanguageAPIImpl;
+import de.lars.apimanager.apis.limitAPI.LimitAPIImpl;
+import de.lars.apimanager.apis.nickAPI.NickAPIImpl;
+import de.lars.apimanager.apis.playerAPI.PlayerAPIImpl;
+import de.lars.apimanager.apis.prefixAPI.PrefixAPIImpl;
+import de.lars.apimanager.apis.questAPI.QuestAPIImpl;
+import de.lars.apimanager.apis.rankAPI.RankAPIImpl;
+import de.lars.apimanager.apis.statusAPI.StatusAPIImpl;
+import de.lars.apimanager.apis.timerAPI.TimerAPIImpl;
+import de.lars.apimanager.apis.toggleAPI.ToggleAPIImpl;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -17,49 +22,80 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 public class JoinListener implements Listener {
-
     PlayerAPIImpl playerAPI = Main.getInstance().getPlayerAPI();
-    CoinAPIImpl coinAPI = Main.getInstance().getCoinAPI();
     LanguageAPIImpl languageAPI = Main.getInstance().getLanguageAPI();
-    RankAPIImpl rankAPI = Main.getInstance().getRankAPI();
-    ToggleAPIImpl toggleAPI = Main.getInstance().getToggleAPI();
-    QuestAPIImpl questAPI = Main.getInstance().getQuestAPI();
-    BanAPIImpl banAPI = Main.getInstance().getBanAPI();
-    TimerAPIImpl timerAPI = Main.getInstance().getTimerAPI();
     BackpackAPIImpl backpackAPI = Main.getInstance().getBackpackAPI();
+    LimitAPIImpl limitAPI = Main.getInstance().getLimitAPI();
+    BanAPIImpl banAPI = Main.getInstance().getBanAPI();
+    CourtAPIImpl courtAPI = Main.getInstance().getCourtAPI();
+    RankAPIImpl rankAPI = Main.getInstance().getRankAPI();
+    PrefixAPIImpl prefixAPI = Main.getInstance().getPrefixAPI();
+    StatusAPIImpl statusAPI = Main.getInstance().getStatusAPI();
+    NickAPIImpl nickAPI = Main.getInstance().getNickAPI();
+    ToggleAPIImpl toggleAPI = Main.getInstance().getToggleAPI();
+    CoinAPIImpl coinAPI = Main.getInstance().getCoinAPI();
+    QuestAPIImpl questAPI = Main.getInstance().getQuestAPI();
+    TimerAPIImpl timerAPI = Main.getInstance().getTimerAPI();
 
     @EventHandler(priority = EventPriority.HIGHEST)
     public void onJoin(PlayerJoinEvent e) {
         Player player = e.getPlayer();
 
-           if (!playerAPI.doesUserExist(player)) {
         if (!playerAPI.doesUserExist(player)) {
             playerAPI.initPlayer(player);
             playerAPI.setOnlineAsync(player, true);
         }
-        if (!coinAPI.doesUserExist(player)) {
-            coinAPI.initPlayer(player);
-        }
+
         if (!languageAPI.doesUserExist(player)) {
             languageAPI.initPlayer(player);
         }
-        if (!rankAPI.doesUserExist(player)) {
-            rankAPI.initPlayer(player);
+
+        if (!backpackAPI.doesUserExist(player)) {
+            backpackAPI.initPlayer(player);
         }
-        if (!toggleAPI.doesUserExist(player)) {
-            toggleAPI.initPlayer(player);
+
+        if (!limitAPI.doesUserExist(player)) {
+            limitAPI.initPlayer(player);
         }
-        if (!questAPI.doesUserExist(player)) {
-            questAPI.initPlayer(player);
-        }
+
         if (!banAPI.doesUserExist(player)) {
             banAPI.initPlayer(player);
         }
+
+        if (!courtAPI.doesUserExist(player)) {
+            courtAPI.initPlayer(player);
+        }
+
+        if (!rankAPI.doesUserExist(player)) {
+            rankAPI.initPlayer(player);
+        }
+
+        if (!prefixAPI.doesUserExist(player)) {
+            prefixAPI.initPlayer(player);
+        }
+
+        if (!statusAPI.doesUserExist(player)) {
+            statusAPI.initPlayer(player);
+        }
+
+        if (!nickAPI.doesUserExist(player)) {
+            nickAPI.initPlayer(player);
+        }
+
+        if (!toggleAPI.doesUserExist(player)) {
+            toggleAPI.initPlayer(player);
+        }
+
+        if (!coinAPI.doesUserExist(player)) {
+            coinAPI.initPlayer(player);
+        }
+
+        if (!questAPI.doesUserExist(player)) {
+            questAPI.initPlayer(player);
+        }
+
         if (!timerAPI.doesUserExist(player)) {
             timerAPI.initPlayer(player);
-        }
-        if (!backpackAPI.doesUserExist(player)) {
-            backpackAPI.initPlayer(player);
         }
     }
 }
